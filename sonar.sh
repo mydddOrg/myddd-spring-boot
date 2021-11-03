e3b6639bbfd8a76ce1b83fb51f333666f7941f2f
lingen.liu <lingen.liu@gmail.com>
2021-11-03 18:51:33 +0800

    refactor:优化Sonar脚本，将token做成外部变量

--- sonar.sh
+++ sonar.sh
@@ -1,17 +1,11 @@
 #!/bin/sh
 
-type=$1
+token="${SONAR_TOKEN}"
 
-if [ "$type" == "office" ]; then
-    token="a1de27b97e33f4f1c7373373583907cc30672c11"
-elif [ "$type" == "home" ]; then
-    token="ba7888fa58e99000136456095c892eea26759ab7"
-else
-   echo "命令执行有误，请指定环境，'sh sonar.sh home' 或者 'sh sonar.sh office'"
+if [ "$token" == "" ]; then
+   echo "没有读取到SONAR_TOKEN系统变量"
    exit 1
 fi
-
-echo $token
 
 ./gradlew test && echo "测试成功" || exit 2
 ./gradlew sonarqube \
