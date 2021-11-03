517393af1d8d8d3727f356e1f3a10ac373b1cc80
lingen.liu <lingen.liu@gmail.com>
2021-11-03 16:42:33 +0800

    fix:完善SonarQube脚本,以支持不同环境

--- sonar.sh
+++ sonar.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 
-./gradlew test && echo "测试成功" || exit 1
+type=$1
+
+if [ "$type" == "office" ]; then
+    token="a1de27b97e33f4f1c7373373583907cc30672c11"
+elif [ "$type" == "home" ]; then
+    token="ba7888fa58e99000136456095c892eea26759ab7"
+else
+   echo "命令执行有误，请指定环境，'sh sonar.sh home' 或者 'sh sonar.sh office'"
+   exit 1
+fi
+
+echo $token
+
+./gradlew test && echo "测试成功" || exit 2
 ./gradlew sonarqube \
   -Dsonar.projectKey=myddd-java \
   -Dsonar.host.url=http://localhost:9000 \
-  -Dsonar.login=ba7888fa58e99000136456095c892eea26759ab7
-
-# office
-# a1de27b97e33f4f1c7373373583907cc30672c11
+  -Dsonar.login=$token
