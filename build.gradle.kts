78365697ba1886e6b6cc9566a9f52f3b28109904
lingen liu <lingen.liu@gmail.com>
2021-11-09 10:30:47 +0800

    release:发布2.0.0-alpha版本

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     id("org.sonarqube") version "3.3"
 }
 
-val projectVersion = "2.0.0-SNAPSHOT"
+val projectVersion = "2.0.0-alpha"
 
 extra["projectVersion"] = projectVersion
 extra["slf4jVersion"] = "1.7.32"
--- sonar.sh
+++ sonar.sh
@@ -5,6 +5,13 @@ token="${SONAR_TOKEN}"
 if [ "$token" == "" ]; then
    echo "没有读取到SONAR_TOKEN系统变量"
    exit 1
+
+fi
+
+check=0
+
+if [ "$check" == "0" ]; then
+    echo "hello"
 fi
 
 ./gradlew test && echo "测试成功" || exit 2
