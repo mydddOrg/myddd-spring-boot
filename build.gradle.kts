a8318bd7787c32d4debf0a857617fc67942ffaee
lingen liu <lingen.liu@gmail.com>
2021-11-17 14:08:09 +0800

    feat: 更新版本至2.0.0-alpha1

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     id("org.sonarqube") version "3.3"
 }
 
-val projectVersion = "2.0.0-alpha"
+val projectVersion = "2.0.0-alpha1"
 
 extra["projectVersion"] = projectVersion
 extra["slf4jVersion"] = "1.7.32"
