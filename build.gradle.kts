ccc3075f3c5f18e88b7d9be745de85235f8460a8
御剑 <lingen.liu@gmail.com>
2023-01-09 12:54:45 +0800

    fix: 更换服务器

--- build.gradle.kts
+++ build.gradle.kts
@@ -144,8 +144,8 @@ subprojects {
                     repositories {
                         maven {
 
-                            val releasesRepoUrl = "sftp://ssh.myddd.org:10010/repositories/releases"
-                            val snapshotsRepoUrl = "sftp://ssh.myddd.org:10010/repositories/snapshots"
+                            val releasesRepoUrl = "sftp://api.myddd.org:10010/repositories/releases"
+                            val snapshotsRepoUrl = "sftp://api.myddd.org:10010/repositories/snapshots"
                             version = projectVersion
                             url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
 
